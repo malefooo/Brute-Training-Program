615b75b18e864855bff6448cd8891a90798d29db
AndrewJiao <646190632@qq.com>
2022-12-01 23:34:58 +0800

    准备手写创建一个运行时

--- common/src/runner/mod.rs
+++ common/src/runner/mod.rs
@@ -2,17 +2,17 @@ mod msg;
 
 struct  Worker<'a> {
     work_id: &'a str,
-    content: TransType
+    content: trans_type
 }
 
 impl Worker {
-    pub fn new(work_id: &str, content: TransType) -> Self {
+    pub fn new(work_id: &str, content: trans_type) -> Self {
         Self { work_id, content }
     }
 }
 
-type TransType = Box<dyn FnOnce() + Send>;
+type trans_type = Box<dyn FnOnce() + Send>;
 enum TransformMsg{
-    SOME(TransType),
+    SOME(trans_type),
     NONE
 }
