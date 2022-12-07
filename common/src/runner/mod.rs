0ffce964ad977524033eff4010f59825485594b2
o-andrew.jiao <o-andrew.jiao@dji.com>
2022-12-07 19:38:04 +0800

    style: clear un use code

--- common/src/runner/mod.rs
+++ common/src/runner/mod.rs
@@ -1,33 +1,3 @@
-
-
 mod msg;
 mod thread_pool;
-mod thread_pool_test;
 
-// struct Service<'a> {
-//     pool: Vec<Worker<'a>>,
-//     name: &'a str,
-// }
-
-
-// impl<'a> Service<'a> {
-//     pub fn new(&self, size: u32, name: &'a str) -> Self {
-//         let mut vec = Vec::new();
-//         let (sender, receiver) = mpsc::channel::<TransformMsg>();
-//         let mutex_receiver = Mutex::new(receiver);
-//         let arc = Arc::new(mutex_receiver);
-//         for i in 0..size {
-//             let arc_receiver = Arc::clone(&arc);
-//             vec.push(Worker::new(name + "_" + i, arc_receiver))
-//         }
-//         self {
-//             vec,
-//             name,
-//         }
-//     }
-// }
-//
-// enum TransformMsg {
-//     SOME(TransType),
-//     NONE,
-// }
